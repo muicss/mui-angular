d6b6a66b9992dcccf4270cc1ceb9fce3672ee707
Jeff Rogers <jefrog1844@gmail.com>
2020-09-08 22:25:26 -0500

    add ripple, disabled, use renderer, optional operators, add test case, modify linting rules

--- projects/mui-angular/button/button.component.spec.ts
+++ projects/mui-angular/button/button.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let button: HTMLButtonElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ButtonComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    button = fixture.nativeElement.querySelector('button');
+    component.variant = 'raised';
+    component.color = 'primary';
+    component.size = 'large';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('button should have class mui-btn', () => {
+    expect(button).toHaveClass('mui-btn');
+  });
+
+  it('button should be variant raised', () => {
+    expect(button).toHaveClass('mui-btn--raised');
+  });
+
+  it('button should be color primary', () => {
+    expect(button).toHaveClass('mui-btn--primary');
+  });
+
+  it('button should be size large', () => {
+    expect(button).toHaveClass('mui-btn--large');
+  });
+
 });
